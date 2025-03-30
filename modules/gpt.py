f7861dd5a8293f061c909dc20369376381bc34a3
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-03-30 14:58:34 -0700

    Init Commit

--- modules/gpt.py
+++ modules/gpt.py
@@ -4,7 +4,7 @@ from modules.env import OPENAI_API_KEY
 openai.api_key = OPENAI_API_KEY
 
 def generate_image(prompt):
-    response = openai.Image.create(
+    response = openai.images.generate(
         prompt=f"A cute {prompt}. Rendered as a cartoon in soft tone.",
         n=1,
         size="1024x1024"
