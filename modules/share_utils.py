b4129d2dffb157de04803381f0613df7fc0b600b
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-03-30 17:31:53 -0700

    Init Commit

--- modules/share_utils.py
+++
@@ -1,6 +0,0 @@
-
-def generate_download_link(image_url):
-    return f'<a href="{image_url}" download="ghibli_image.png" target="_blank">📥 Download Image</a>'
-
-def generate_instagram_link(image_url):
-    return f'<p>To share on Instagram: save the image and open the Instagram app to post manually.<br><a href="{image_url}" target="_blank">📸 View Full Image</a></p>'
