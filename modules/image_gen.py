65a01e0cefdfa173261fbe1ded387a5c6953dd27
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-03-30 17:18:31 -0700

    Init Commit

--- modules/image_gen.py
+++ modules/image_gen.py
@@ -1,15 +1,12 @@
 import openai
-import os
+import streamlit as st
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 def generate_image(prompt):
     response = openai.images.generate(
-        prompt=f"Convert this {prompt} into a modern anime style with"
-               " Ghibli influence, clean line art, realistic shading,"
-               " soft pastel tones, and expressive faces."
-               " Inspired by scenes from 'Your Name' and 'Whisper of the Heart'."
-               " Emphasize clarity, color harmony, and emotional warmth.",
+        model="dall-e-3",
+        prompt=f"Convert this {prompt} into a modern anime style with Ghibli influence, clean line art, realistic shading, soft pastel tones, and expressive faces. Inspired by scenes from 'Your Name' and 'Whisper of the Heart'. Emphasize clarity, color harmony, and emotional warmth.",
         n=1,
         size="1024x1024"
     )
