84de66540ca8eed01d36d0f5957f5bf30b3e12bf
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-03-31 12:09:51 -0700

    Init Commit

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ openai.api_key = OPENAI_API_KEY
 s3 = boto3.client("s3", region_name=AWS_REGION)
 
 st.set_page_config(page_title="GPT-4o Image Wrapper", layout="centered")
-st.title("🖼️ GPT-4o Prompt/Image to Anime")
+st.title("🖼️ You deserve an almost-ghiblied version of you!")
 
-uploaded_image = st.file_uploader("📤 Upload an image (optional)", type=["png", "jpg", "jpeg"])
-prompt = st.text_area("Or enter a text prompt")
+uploaded_image = st.file_uploader("📤 Upload an image here (optional)", type=["png", "jpg", "jpeg"])
+prompt = st.text_area("Or write a text prompt to ghiblify it")
 
 
 client = genai.Client(api_key=GEMINI_API_KEY)
