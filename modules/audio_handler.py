60b2fb94429ba83cb10b058abb10e7c38c61467e
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-03-30 17:08:46 -0700

    Init Commit

--- modules/audio_handler.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from st_audiorec import st_audiorec
-
-def record_audio():
-    return st_audiorec()
--- modules/share_utils.py
+++
@@ -1,5 +0,0 @@
-def generate_download_link(image_url):
-    return f'<a href="{image_url}" download="ghibli_image.png" target="_blank">📥 Download Image</a>'
-
-def generate_instagram_link(image_url):
-    return f'<a href="{image_url}" target="_blank">📸 Share on Instagram</a>'
