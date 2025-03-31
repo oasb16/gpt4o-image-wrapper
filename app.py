5f93dce0cbac9a5cb0d40d16a4bf532fe6eafaa5
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-03-30 21:10:41 -0700

    Init Commit

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if st.button("Generate / Upload") and (prompt or uploaded_image):
             print(f"img_bytes : {img_bytes}")
             file_id = f"user_uploads/{uuid.uuid4()}.png"
             print(f"file_id : {file_id}")
-            image_url = generate_image_from_image
+            image_url = generate_image_from_image()
             st.success(f"Uploaded s3_url to S3: {image_url}")
             s3.upload_fileobj(BytesIO(img_bytes), S3_BUCKET, file_id)
             s3_url_2 = f"https://{S3_BUCKET}.s3.{AWS_REGION}.amazonaws.com/{file_id}"            
